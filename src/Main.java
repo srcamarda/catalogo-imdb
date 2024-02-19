ce7d168f090998cca1de3b84a3c31450f09fcb20
Thiago Camarda <thiago.camarda@gmail.com>
2024-02-19 20:37:51 -0300

    Initial menus sketch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
+import catalog.*;
+import utils.*;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int menuOption, subMenuOption;
+
+        do {
+            //Show the initial menu
+            Menu.showMenu();
+            System.out.print("Selecione uma opção: ");
+
+            menuOption = input.nextInt();
+            input.nextLine();
+
+            if (menuOption < 1 || menuOption > 4) {
+                System.out.print("\nOpção inválida selecionada! Enter para continuar...");
+                input.nextLine();
+                continue;
+            }
+
+            switch (menuOption) {
+                case 1: //Search movie
+                    //searchMovie();
+                    break;
+
+                case 2: //List all movies
+                    //listMovies();
+                    break;
+
+                case 3: //Register options
+                    do {
+                        //Show the sub menu
+                        Menu.showOptions();
+                        System.out.print("Selecione uma opção: ");
+
+                        subMenuOption = input.nextInt();
+                        input.nextLine();
+
+                        if (subMenuOption < 1 || subMenuOption > 6) {
+                            System.out.print("\nOpção inválida selecionada! Enter para continuar...");
+                            input.nextLine();
+                            continue;
+                        }
+
+                        switch (subMenuOption) {
+                            case 1: //Add movie
+                                //addMovie();
+                                break;
+                            case 2: //Add actor
+                                //addActor();
+                                break;
+                            case 3: //Add director
+                                //addDirector();
+                                break;
+                            case 4: //Add genre
+                                //addGenre();
+                                break;
+                            case 5: //Add associations
+                                //addAssociation();
+                                break;
+                        }
+
+                    } while (subMenuOption != 6);
+                    break;
+
+                case 4: //Exit
+                    System.out.println("\nAté mais!");
+            }
+
+        } while (menuOption != 4);
+
+        input.close();
     }
 }
--- src/utils/Menu.java
+++ src/utils/Menu.java
@@ -1,4 +1,35 @@
 package utils;
 
 public class Menu {
+    public static void showMenu(){
+        String menuStr = """
+                
+                #########################
+                ##### CATÁLOGO IMDB #####
+                #########################
+                                
+                >>>> Menu <<<<
+                1 - Pesquisar filme
+                2 - Lista de filmes
+                3 - Opções de cadastro
+                4 - Sair
+                """;
+
+        System.out.println(menuStr);
+    }
+
+    public static void showOptions(){
+        String menuStr = """
+                      
+                >>>> Cadastro <<<<
+                1 - Cadastrar filme
+                2 - Cadastrar ator
+                3 - Cadastrar diretor
+                4 - Cadastrar gênero
+                5 - Adicionar associações
+                6 - Voltar
+                """;
+
+        System.out.println(menuStr);
+    }
 }
