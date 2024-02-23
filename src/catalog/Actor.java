6dc7efd0f8ce42557c729349c9ed9d3e900dffcb
JORGEABARROCO <153916367+JORGEABARROCO@users.noreply.github.com>
2024-02-23 09:15:58 -0300

    Update Actor.java
     pacote alterado para "catalogo", que é mais descritivo do que "catalog"
    adicionado o atributo "nomeArtistico", que é importante para identificar um ator
    método construtor foi modificado para incluir o novo atributo "nomeArtistico"
    método "toString()" foi sobrescrito para retornar uma representação textual mais completa do objeto "Ator"

--- src/catalog/Actor.java
+++ src/catalog/Actor.java
@@ -1,7 +1,28 @@
-package catalog;
+package catalogo;
 
-public class Actor extends Person {
-    public Actor(String name, String cpf) {
-        super(name, cpf);
+public class Ator extends Pessoa {
+
+    private String nomeArtistico;
+
+    public Ator(String nome, String cpf, String nomeArtistico) {
+        super(nome, cpf);
+        this.nomeArtistico = nomeArtistico;
+    }
+
+    public String getNomeArtistico() {
+        return nomeArtistico;
+    }
+
+    public void setNomeArtistico(String nomeArtistico) {
+        this.nomeArtistico = nomeArtistico;
+    }
+
+    @Override
+    public String toString() {
+        return "Ator{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", nomeArtistico='" + nomeArtistico + '\'' +
+                '}';
     }
 }
