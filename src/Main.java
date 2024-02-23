db51b4c99c8298b3686fb6f03c70efbb22f43b78
JORGEABARROCO <153916367+JORGEABARROCO@users.noreply.github.com>
2024-02-23 09:17:25 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-import catalogView.Interactivity;
+package catalogView;
 
-public class Main {
-    public static void main(String[] args) {
-        Interactivity.mainOptions();
+import catalog.*;
+import catalogController.*;
+import utils.*;
+import java.util.Scanner;
+
+public class Interactivity {
+    static Scanner scanner = new Scanner(System.in);
+    static DirectorController directorController = new DirectorController();
+    static ActorController actorController = new ActorController();
+    static MovieController movieController = new MovieController();
+
+    public static Movie searchMovie() {
+        System.out.println("Digite o nome do filme");
+        String userInputMovieName = scanner.nextLine();
+        return movieController.searchMovie(userInputMovieName);
     }
-}
